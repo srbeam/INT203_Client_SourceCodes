6718918717fc56d01a9ad9ec7cef5042f56cc31d
srbeam <sarisa.beam@mail.kmutt.ac.th>
2022-02-11 03:36:22 +0700

    update

---
+++ basic-dynamic-style/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+const warningColor = "red";
+const warningStyle = "italic";
+const successText = "text-decoration:underline";
+const isEmpty = false;
+const hasDecor = true;
+</script>
+
+<template>
+  <div>
+    <p :style="{ color: warningColor, 'font-style': warningStyle }">
+      This is warning text.
+    </p>
+
+    <p :style="isEmpty ? { color: 'red' } : { color: 'green' }">
+      This is a style under condition
+    </p>
+    <p :style="[successText]">This is a success text</p>
+    <p :style="[successText, decorText]">This is a success text</p>
+    <p :style="[successText, hasDecor ? decorText : '']">
+      This is styles under condition
+    </p>
+  </div>
+</template>
+
+<style>
+</style>
