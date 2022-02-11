403252a7aed09b298b190a01efa2256ecc141fd4
srbeam <sarisa.beam@mail.kmutt.ac.th>
2022-02-11 16:49:22 +0700

    v-bind

---
+++ directive-template/src/App-v-bind.vue
@@ -0,0 +1,17 @@
+<script setup>
+const headingStyle = "color: red"
+const paraStyle = "text-decoration: underline" 
+const imagePath = "../public/images/mypic..jpg"
+</script>
+ 
+<template>
+<h1 :style ="headingStyle"> INT203 Client side </h1>
+<p :style ="paraStyle"> Umaporn </p>
+<img src = "./assets/logo.png">
+<img :src="imagePath " alt="pic" srcset="">
+
+</template>
+ 
+<style>
+
+</style>
