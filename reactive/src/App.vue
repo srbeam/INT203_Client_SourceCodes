8b8b31c8d95c11e25f484986045194dedc0ba417
srbeam <sarisa.beam@mail.kmutt.ac.th>
2022-02-13 00:19:43 +0700

    reactive

---
+++ reactive/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+console.clear()
+const counter = ref(1)
+const isEmpty = ref(false)
+// setTimeout(() => console.log(++counter), 3000)
+// setInterval(() => console.log(++counter.value), 1000)
+</script>
+
+<template>
+  <p>Counter: {{ counter }}</p>
+  <p>Empty: {{ isEmpty }}</p>
+
+</template>
+
+<style></style>
